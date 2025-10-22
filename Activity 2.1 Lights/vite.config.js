3af373d5b5fd34870d921a623d355f9ff83fd4a3
RossanB <bbalajeboco.bb@gmail.com>
2025-10-22 23:13:41 +0800

    Vite update

--- Activity 2.1 Lights/vite.config.js
+++ Activity 2.1 Lights/vite.config.js
@@ -1,18 +1,18 @@
+import { defineConfig } from "vite"
+
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
-export default {
+export default defineConfig({
     root: 'src/',
     publicDir: '../static/',
-    base: './',
-    server:
-    {
+    base: './', // IMPORTANT: Keeps assets relative, works on Vercel/GitHub pages
+    server: {
         host: true,
         open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
     }
-}
+})
